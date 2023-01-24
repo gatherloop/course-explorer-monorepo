01e1c55813a2eca39564b2678039b83e16119480
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2023-01-25 06:58:54 +0700

    chore: move apps folder

--- apps/ui/cms/jest.config.ts
+++ apps/ui/cms/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'user-facing',
-  preset: '../../jest.preset.js',
+  displayName: 'ui-cms',
+  preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/user-facing',
+  coverageDirectory: '../../../coverage/apps/ui/cms',
 };
